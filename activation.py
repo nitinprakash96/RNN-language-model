e7974f8d72c0009ce6e165951fa1d6ac448953fc
nitinprakash96 <prakash.nitin63@gmail.com>
2018-08-20 01:25:25 +0530

    blueprint for activations

---
+++ activation.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class tanh:
+    def forward_pass():
+        pass
+
+    def backward_pass():
+        pass
+
+class sigmoid:
+    def forward_pass():
+        pass
+
+    def backward_pass():
+        pass
+
+class relu:
+    def forward_pass():
+        pass
+
+    def backward_pass():
+        pass
