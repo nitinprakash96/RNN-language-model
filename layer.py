de5ec849fdd81b275587bbe4236a7ffd9c42f734
nitinprakash96 <prakash.nitin63@gmail.com>
2018-08-20 23:57:38 +0530

    blueprint for RNN layer

---
+++ layer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from activation import tanh, sigmoid, relu
+from node import MultiplyNode, SumNode
+
+mul = MultiplyNode()
+add = SumNode()
+activation = sigmoid()
+
+class Layer:
+    def forward():
+        pass
+
+    def backward():
+        pass
