51bbcc5f17041905da9294f18eb54cc82270f760
nitinprakash96 <prakash.nitin63@gmail.com>
2018-08-20 23:57:14 +0530

    activations

--- activation.py
+++ activation.py
@@ -1,22 +1,40 @@
 import numpy as np
 
 class tanh:
-    def forward_pass():
-        pass
+    """
+    This activation squashes the output in the range -1 and 1.
+    """
+    def forward_pass(self, bottom):
+        output = np.tanh(bottom)
+        return output
 
-    def backward_pass():
-        pass
+    def backward_pass(self, top):
+        x = forward_pass(top)
+        output = (1.0 - np.square(x))
+        return 
 
 class sigmoid:
-    def forward_pass():
-        pass
+    """
+    This activation squashes the output in the range 0 and 1.
+    """
+    def forward_pass(self, bottom):
+        output = 1.0 / (1.0 * np.exp(-bottom))
+        return output
 
-    def backward_pass():
-        pass
+    def backward_pass(self, top):
+        x = forward_pass(top)
+        output = x * (1.0 - x)
 
+# source: https://stackoverflow.com/a/47936476/6244324
 class relu:
-    def forward_pass():
-        pass
+    """
+    This activation gives an output x if x is positive and 0 otherwise.
+    Mathematically this can be represented as: A(x) = max(0,x)
+    """
+    def forward_pass(self, bottom):
+        output = bottom * (bottom > 0)
+        return output
 
-    def backward_pass():
-        pass
+    def backward_pass(self, top):
+        output = 1.0 * (top > 0)
+        return output
